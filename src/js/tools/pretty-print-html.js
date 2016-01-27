9db5ff800930a2849215f55181411c8130a7fdd2
Jess Telford <jess+github@jes.st>
2016-01-28 08:27:53 +1100

    Better Pretty Printing of HTML

--- src/js/tools/pretty-print-html.js
+++ src/js/tools/pretty-print-html.js
@@ -3,8 +3,10 @@ var he = require('he'),
 
 module.exports = function prettyPrintHtml(html, cropLength = 20, delimiter = '...') {
   return htmlStringToNodesArray(html).map(el => {
-    var wrappingTags = el.outerHTML.split(el.innerHTML);
     // TODO: Do this in CSS with overflow:elipses
+    var wrappingTags = el.outerHTML.split('>' + el.innerHTML + '<');
+    wrappingTags[0] = wrappingTags[0] + '>';
+    wrappingTags[1] = '<' + wrappingTags[1];
     if (wrappingTags[0].length > cropLength) {
       wrappingTags[0] = wrappingTags[0].slice(0, cropLength - (delimiter.length + 1)) + delimiter + '>';
     }
